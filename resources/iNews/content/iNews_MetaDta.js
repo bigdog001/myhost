3bc41b54bed725535706f5a71dfe620dd79d32a7
root <294356967@qq.com>
2024-01-17 22:01:44 +0000

    init project

---
+++ resources/iNews/content/iNews_MetaDta.js
@@ -0,0 +1,26 @@
+const totalRecordsSize={
+    totalRecordsSize:480,
+    channelRecordsSize:[12,145,19,88],
+    categories:["","","","","",""],
+    channelContents:[
+        [
+         {title:"News title 1.channel1",abstracts:"News abstracts1.channel1",content:" full content1.channel1....",author:"HelloGuy",publishDate:["2024","01","11","08:11"],category:[1,3]},
+         {title:"News title 2.channel1",abstracts:"News abstracts2.channel1",content:" full content2.channel1....",author:"HelloGuy",publishDate:["2024","12","18","14:23"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+         {title:"News title 4.channel1",abstracts:"News abstracts4.channel1",content:" full content4.channel1....",author:"HelloGuy",publishDate:["2024","12","18","23:56"],category:[1,3]}
+        ],
+        [
+            {title:"News title 1.channel2",abstracts:"News abstracts1.channel2",content:" full content1.channel2....",author:"HelloGuy",publishDate:["2024","01","11","08:11"],category:[1,3]},
+            {title:"News title 2.channel2",abstracts:"News abstracts2.channel2",content:" full content2.channel2....",author:"HelloGuy",publishDate:["2024","12","18","14:23"],category:[1,3]},
+            {title:"News title 3.channel2",abstracts:"News abstracts3.channel2",content:" full content3.channel2....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+            {title:"News title 4.channel2",abstracts:"News abstracts4.channel2",content:" full content4.channel2....",author:"HelloGuy",publishDate:["2024","12","18","23:56"],category:[1,3]}
+        ],
+        [
+            {title:"News title 1.channel3",abstracts:"News abstracts1.channel3",content:" full content1.channel3....",author:"HelloGuy",publishDate:["2024","01","11","08:11"],category:[1,3]},
+            {title:"News title 2.channel3",abstracts:"News abstracts2.channel3",content:" full content2.channel3....",author:"HelloGuy",publishDate:["2024","12","18","14:23"],category:[1,3]},
+            {title:"News title 3.channel3",abstracts:"News abstracts3.channel3",content:" full content3.channel3....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+            {title:"News title 4.channel3",abstracts:"News abstracts4.channel3",content:" full content4.channel3....",author:"HelloGuy",publishDate:["2024","12","18","23:56"],category:[1,3]}
+        ]
+    ]
+
+}
