08ba869c0986e1c7c5ba0d48c131b166ad4d4a9d
root <294356967@qq.com>
2024-01-28 03:15:57 +0000

    init project

--- resources/iNews/content/iNews_MetaDta.js
+++ resources/iNews/content/iNews_MetaDta.js
@@ -1,11 +1,36 @@
 const totalRecords={
     totalRecordsSize:480,
     channelRecordsSize:[12,145,19,88],
-    categories:["","","","","",""],
+    categories:["TRAVEL","FOOD & DRINK","TV&MOVIES","LIFESTYLE","TEST","WILD"],
     channelContents:[
         [
-         {title:"News title 1.channel1",abstracts:"News abstracts1.channel1",content:" full content1.channel1....",author:"HelloGuy",publishDate:["2024","01","11","08:11"],category:[1,3]},
+         {
+             id:1,
+             title:"News title 1.channel1",
+             abstracts:"News abstracts1.channel1",
+             content:" full content1.channel1....",
+             author:"HelloGuy",
+             publishDate:["2024","01","11","08:11"],
+             category:[1,3],
+             images:[
+                 {src:"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",desc:"......."},
+                 {src:"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",desc:"......."}
+             ]
+         },
+
+
          {title:"News title 2.channel1",abstracts:"News abstracts2.channel1",content:" full content2.channel1....",author:"HelloGuy",publishDate:["2024","12","18","14:23"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
+         {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
          {title:"News title 3.channel1",abstracts:"News abstracts3.channel1",content:" full content3.channel1....",author:"HelloGuy",publishDate:["2024","02","11","23:43"],category:[1,3]},
          {title:"News title 4.channel1",abstracts:"News abstracts4.channel1",content:" full content4.channel1....",author:"HelloGuy",publishDate:["2024","12","18","23:56"],category:[1,3]}
         ],
