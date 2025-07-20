e394eb78c41cfa7d400fb38fc167f5d546a0c2ca
bigdog001 <bigdog001@users.noreply.github.com>
2025-07-19 22:46:17 -0500

    Update 100.js

--- resources/buddha/FlowerOrnamentScripture/js/100.js
+++ resources/buddha/FlowerOrnamentScripture/js/100.js
@@ -223,9 +223,58 @@ const scriptureRecords={
                 "itemComment":" ",
                 "good":true,
                },
-    {"itemPrefix":" ","item":"又如长风起","itemPostFix":" ,","itemTrans":"Also like a continuous wind","itemComment":"","good":true},
+    {"itemPrefix":"   \"","item":"又如长风起","itemPostFix":" ,","itemTrans":"Also like a continuous wind","itemComment":"","good":true},
     {"itemPrefix":" ","item":"遇物咸鼓扇","itemPostFix":" ,","itemTrans":"Fanning and drumming whatever it hits","itemComment":"","good":true},
     {"itemPrefix":" ","item":"各各不相知","itemPostFix":", ","itemTrans":"Each unaware of the other","itemComment":"","good":true},
-     {"itemPrefix":" ","item":"诸法亦如是","itemPostFix":".<br/><br/>","itemTrans":"So also are all things","itemComment":"","good":true}
+    {"itemPrefix":" ","item":"诸法亦如是","itemPostFix":".<br/><br/>","itemTrans":"So also are all things","itemComment":"","good":true},
+    
+    {"itemPrefix":"    \"","item":"又如众地界","itemPostFix":" ,","itemTrans":"Also like the various levels of earth","itemComment":"","good":true},
+    {"itemPrefix":"","item":"展转因依住","itemPostFix":" ,","itemTrans":"Each based on another","itemComment":"","good":true},
+    {"itemPrefix":"","item":"各各不相知","itemPostFix":" ,","itemTrans":"Yet unaware of the others","itemComment":"","good":true},
+    {"itemPrefix":"","item":"诸法亦如是","itemPostFix":" .<br/><br/>","itemTrans":"Thus are all phenomena","itemComment":"","good":true},
+
+    {"itemPrefix":"    \"","item":"眼耳鼻舌身","itemPostFix":",","itemTrans":"Eye, car, nose, tongue, body","itemComment":"","good":true},
+    {"itemPrefix":"","item":"心意诸情根","itemPostFix":",","itemTrans":"Mind, intellect, the faculties of sense","itemComment":"","good":true},
+    {"itemPrefix":" ","item":"以此常流转","itemPostFix":",","itemTrans":"By these one always revolves","itemComment":"revolves-旋转","good":true},
+    {"itemPrefix":" ","item":"而无能转者","itemPostFix":".<br/><br/>","itemTrans":"Yet there is no one, nothing that revolves","itemComment":"","good":true},
+
+    {"itemPrefix":"    \"","item":"法性本无生","itemPostFix":",","itemTrans":"The nature of things is fundamentally birthless","itemComment":"","good":true},
+    {"itemPrefix":" ","item":"示现而有生","itemPostFix":",","itemTrans":"Yet they appear to have birth","itemComment":"","good":true},
+    {"itemPrefix":" ","item":"是中无能现","itemPostFix":",","itemTrans":"Herein there is no revealer","itemComment":"revealer-揭露者","good":true},
+    {"itemPrefix":" ","item":"亦无所现物","itemPostFix":".<br/><br/>","itemTrans":"And nothing that's revealed","itemComment":"","good":true},
+
+    {"itemPrefix":"     \"","item":"眼耳鼻舌身","itemPostFix":",","itemTrans":"Eye, car, nose, tongue, body","itemComment":"","good":true},
+    {"itemPrefix":" ","item":"心意诸情根","itemPostFix":",","itemTrans":"Mind, intellect, the faculties of sense","itemComment":"","good":true},
+    {"itemPrefix":" ","item":"一切空无性","itemPostFix":",","itemTrans":"All are void and essenceless","itemComment":"","good":true},
+    {"itemPrefix":" ","item":"妄心分别有","itemPostFix":".<br/><br/>","itemTrans":"The deluded mind conceives them to exist","itemComment":"","good":true},
+
+    {"itemPrefix":"     \"","item":"如理而观察","itemPostFix":",","itemTrans":"Seen as they truly are","itemComment":"","good":true},
+    {"itemPrefix":" ","item":"一切皆无性","itemPostFix":",","itemTrans":"All arc without inherent nature","itemComment":"","good":true},
+    {"itemPrefix":" ","item":"法眼不思议","itemPostFix":",","itemTrans":"The eye of reality is not conceptual","itemComment":"","good":true},
+    {"itemPrefix":" ","item":"此见非颠倒","itemPostFix":".<br/><br/>","itemTrans":"This seeing is not false","itemComment":"","good":true},
+
+    {"itemPrefix":"    \"","item":"若实若不实","itemPostFix":",","itemTrans":"Real or unreal","itemComment":"","good":true},
+    {"itemPrefix":" ","item":"若妄若非妄","itemPostFix":",","itemTrans":"False or not false","itemComment":"","good":true},
+    {"itemPrefix":" ","item":"世间出世间","itemPostFix":",","itemTrans":"Mundane or transmundane","itemComment":"transmundane-超凡脱俗","good":true},
+    {"itemPrefix":" ","item":"但有假言说","itemPostFix":".<br/><br/>","itemTrans":"There's nothing but descriptions","itemComment":"","good":true},
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ]
 };
