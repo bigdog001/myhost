f14dc78da5e40c64880fad7883f44d4a0c84c571
bigdog001 <jiunian.wang@gmail.com>
2025-08-05 16:54:50 -0500

    ..

---
+++ resources/buddha/FlowerOrnamentScripture/js/111.js
@@ -0,0 +1,15 @@
+const scriptureRecords={
+"id":11,
+"title":"第11品 净行",
+"data":[
+    {"itemPrefix":"    ","itemTrans":"尔时,文殊师利菩萨问觉首菩萨言","itemPostFix":" :","item":"THEN THE ENLIGHTENING BEING Manjushri asked the enlightening being Chief of the Awakened","itemComment":"Manjushri-文殊师利菩萨,THE ENLIGHTENING BEING-菩萨,Chief-首席","good":true,},
+
+
+
+
+
+
+
+
+]
+};
