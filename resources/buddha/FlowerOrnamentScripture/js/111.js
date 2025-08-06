e443222364090bfdd1f264a55f326a4dc0c26b7f
bigdog001 <bigdog001@users.noreply.github.com>
2025-08-06 08:47:17 -0500

    Update 111.js

--- resources/buddha/FlowerOrnamentScripture/js/111.js
+++ resources/buddha/FlowerOrnamentScripture/js/111.js
@@ -2,7 +2,18 @@ const scriptureRecords={
 "id":11,
 "title":"第11品 净行",
 "data":[
-    {"itemPrefix":"    ","itemTrans":"尔时,文殊师利菩萨问觉首菩萨言","itemPostFix":" :","item":"THEN THE ENLIGHTENING BEING Manjushri asked the enlightening being Chief of the Awakened","itemComment":"Manjushri-文殊师利菩萨,THE ENLIGHTENING BEING-菩萨,Chief-首席","good":true,},
+    {"itemPrefix":"     ","item":"THEN THE E NLIGHTENING BEING Chief in Knowledge asked the enlightening being Manjushri","itemPostFix":" :\"","itemTrans":"尔时，智首菩萨问文殊师利菩萨言","itemComment":"","good":true},  
+    {"itemPrefix":" ","item":"How can enlightening beings attain faultless physical, verbal, and mental action","itemPostFix":" ?","itemTrans":"佛子，菩萨云何得无过失身、语、意业","itemComment":"","good":true},  
+    {"itemPrefix":" ","item":"How can they attain harmless physical, verbal, and mental action","itemPostFix":" ?","itemTrans":"云何得不害身、语、意业","itemComment":"","good":true},  
+    {"itemPrefix":" ","item":" How can they attain blameless physical, verbal, and mental action","itemPostFix":" ?","itemTrans":"云何得不可毁身、语、意业","itemComment":"","good":true},  
+    {"itemPrefix":" ","item":"How can they attain invulnerable physical, verbal, and mental action","itemPostFix":" ?","itemTrans":"云何得不可坏身、语、意业","itemComment":"","good":true},  
+    {"itemPrefix":" ","item":"How can they attain non-regressive physical, verbal, and mental action","itemPostFix":" ?","itemTrans":"云何得不退转身、语、意业","itemComment":"","good":true},  
+    {"itemPrefix":" ","item":"How can they attain unshakable physical, verbal, and mental action","itemPostFix":" ?","itemTrans":"云何得不可动身、语、意业","itemComment":"","good":true},  
+    {"itemPrefix":" ","item":"How can they attain excellent physical, verbal, and mental action","itemPostFix":" ?","itemTrans":"云何得殊胜身、语、意业","itemComment":"","good":true},  
+    {"itemPrefix":" ","item":" How can they attain pure physical, verbal, and mental action","itemPostFix":" ?","itemTrans":"云何得清净身、语、意业","itemComment":"","good":true},  
+    {"itemPrefix":" ","item":"How can they attain unpolluted physical, verbal, and mental action","itemPostFix":" ?","itemTrans":"云何得无染身、语、意业","itemComment":"","good":true},  
+    {"itemPrefix":" ","item":"How can they attain physical, verbal, and mental action that is guided by wisdom","itemPostFix":" ?","itemTrans":"云何得智为先导身、语、意业","itemComment":"","good":true},  
+    
 
 
 
