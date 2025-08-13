2644040b20d935ea7275361a5f71e9122ca9df44
bigdog001 <bigdog001@users.noreply.github.com>
2025-08-13 11:13:29 -0500

    Update 111.js

--- resources/bible/js/111.js
+++ resources/bible/js/111.js
@@ -1,6 +1,6 @@
 const scriptureRecords={
 "id":11,
-"title":"民数记第26章",
+"title":"马太福音Matthew",
 "data":[
     {"itemPrefix":"     ","item":"THEN THE E NLIGHTENING BEING Chief in Knowledge asked the enlightening being Manjushri","itemPostFix":" :\"","itemTrans":"尔时，智首菩萨问文殊师利菩萨言","itemComment":"","good":true},  
     
